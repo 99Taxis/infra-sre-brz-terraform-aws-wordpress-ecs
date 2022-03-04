67515da2b3a10bf904761575070871492359622a
Jose Augusto Maria <josemaria@99app.com>
2022-03-04 18:28:50 -0300

    ze

--- main.tf
+++ main.tf
@@ -150,25 +150,16 @@ resource "aws_lb" "wordpress" {
   tags               = var.tags
 }
 
-resource "aws_lb_listener" "wordpress_http" {
-  load_balancer_arn = aws_lb.wordpress.arn
-  port              = "80"
-  protocol          = "HTTP"
-    
-    default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.wordpress_http.arn
-    }
-//  default_action {
-//    type = "redirect"
-//
-//    redirect {
-//      port        = "443"
-//      protocol    = "HTTPS"
-//      status_code = "HTTP_301"
+//resource "aws_lb_listener" "wordpress_http" {
+//  load_balancer_arn = aws_lb.wordpress.arn
+//  port              = "80"
+//  protocol          = "HTTP"
+//    
+//    default_action {
+//    type             = "forward"
+//    target_group_arn = aws_lb_target_group.wordpress_http.arn
 //    }
-//  }
-}
+//}
 
 resource "aws_lb_listener" "wordpress_https" {
   count             = var.lb_listener_enable_ssl ? 1 : 0
